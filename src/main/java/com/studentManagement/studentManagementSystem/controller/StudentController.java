fe6ad3a72330384ec1687ad6159310e6bb39dec5
kanu Abubakarr <bakarrstudent1@gmail.com>
2024-06-04 11:08:50 +0000

    an update on the controller

--- src/main/java/com/studentManagement/studentManagementSystem/controller/StudentController.java
+++ src/main/java/com/studentManagement/studentManagementSystem/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.studentManagement.studentManagementSystem.model.domain.Student;
 import com.studentManagement.studentManagementSystem.model.service.StudentManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,6 +16,11 @@ public class StudentController{
     @GetMapping("/student")
     public List<Student> getAllStudent(Student student){
         return studentManager.getAllStudents(student);
+    }
+
+    @GetMapping("/student/{id}")
+    public Student getStudentById(@PathVariable int id){
+        return studentManager.getById(id);
     }
 
 }
