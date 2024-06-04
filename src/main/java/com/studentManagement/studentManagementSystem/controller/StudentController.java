7f96f54eda6d875b7ca786249a3c6ab47043f036
YeaniBee06 <152268478+YeaniBee06@users.noreply.github.com>
2024-06-04 10:55:54 +0000

    Controller updated

--- src/main/java/com/studentManagement/studentManagementSystem/controller/StudentController.java
+++ src/main/java/com/studentManagement/studentManagementSystem/controller/StudentController.java
@@ -1,4 +1,20 @@
 package com.studentManagement.studentManagementSystem.controller;
 
+import com.studentManagement.studentManagementSystem.model.domain.Student;
+import com.studentManagement.studentManagementSystem.model.service.StudentManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class StudentController{
+    @Autowired
+    private StudentManager studentManager;
+    @GetMapping("/student")
+    public List<Student> getAllStudent(Student student){
+        return studentManager.getAllStudents(student);
+    }
+
 }
